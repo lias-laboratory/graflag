5bfc121f810162dce67e108c42abc38d7884be62
bay-ensma <bay127prem.ensma@gmail.com>
2026-07-08 10:10:00 +0200

    Bump version to 1.0.1 for first OIDC-published release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="graflag",
-    version="1.0.0",
+    version="1.0.1",
     description="Distributed benchmarking framework for Graph Anomaly Detection",
     author="gbay7",
     packages=find_packages(),
